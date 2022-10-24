44f468508a179bfd093db42810bfbd5e94deb1fa
Arthur Rosch <rosch.arthur@gmail.com>
2022-10-24 11:27:39 -0300

    Exec finalizado

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 // tamanho de uma barra de chocolate n X m, o objetivo dessa função é quebrar a
 // barra de chocolate em pedaços 1x1 e retornar o número mínimo de quebras
 // necessárias, se algum dos parâmetros de entrada for 0 você deve retornar -1.
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _Lutador_Nome, _Lutador_Vida, _Lutador_Ataque;
 // Calcular(100, 100);
 // function Calcular(Largura: number, altura: number) {
 //   let ContadorQuebras = 0;
@@ -17,17 +29,59 @@
 // minúsculas ou maiúsculas de acordo com a seguinte regra: fazer o mínimo de
 // alterações possíveis, se a contagem de minúsculas e maiúsculas for igual deve
 // retornar apenas minúsculas.
-function verificando(palavra) {
-    var maiúsculas = palavra.replace(/[^A-Z\s]/g, ""), minúsculas = palavra.replace(/[^a-z\s]/g, "");
-    console.log(minúsculas, maiúsculas);
-    if (minúsculas.length == maiúsculas.length) {
-        return palavra.toLocaleLowerCase();
+// function verificando(palavra: string): string {
+//   let maiúsculas = palavra.replace(/[^A-Z\s]/g, ""),
+//     minúsculas = palavra.replace(/[^a-z\s]/g, "");
+//   if (minúsculas.length == maiúsculas.length) {
+//     return palavra.toLocaleLowerCase();
+//   }
+//   if (minúsculas.length < maiúsculas.length) {
+//     return palavra.toUpperCase();
+//   }
+//   if (minúsculas.length > maiúsculas.length) {
+//     return palavra.toLocaleLowerCase();
+//   }
+// }
+// console.log(verificando("artHUR"));
+// 3. Crie uma função que retorne o nome do vencedor em uma luta entre dois lutadores.
+// Cada lutador ataca o adversário no seu próprio turno, quem derrubar o outro
+// primeiro vence, um jogador é derrubado quando sua vida for <= 0.
+// Cada lutador é um objeto de uma classe Lutador, portanto você deve criá-lo com as
+// seguintes propriedades: Nome: string, Vida: number, Ataque: number.
+// Tanto vida como ataque devem ser valores inteiros maiores que 0 e a função além
+// dos dois lutadores deve receber um terceiro parâmetro com o nome de quem vai
+// começar atacando.
+var lutadores = [];
+var Lutador = /** @class */ (function () {
+    function Lutador(nome, vida, ataque) {
+        _Lutador_Nome.set(this, void 0);
+        _Lutador_Vida.set(this, void 0);
+        _Lutador_Ataque.set(this, void 0);
+        this.setNome(nome);
+        this.setVida(vida);
+        this.setAtaque(ataque);
     }
-    if (minúsculas.length < maiúsculas.length) {
-        return palavra.toUpperCase();
-    }
-    if (minúsculas.length > maiúsculas.length) {
-        return palavra.toLocaleLowerCase();
-    }
-}
-console.log(verificando("artHUR"));
+    Lutador.prototype.setNome = function (nome) {
+        __classPrivateFieldSet(this, _Lutador_Nome, nome, "f");
+    };
+    Lutador.prototype.setVida = function (vida) {
+        __classPrivateFieldSet(this, _Lutador_Vida, vida, "f");
+    };
+    Lutador.prototype.setAtaque = function (ataque) {
+        __classPrivateFieldSet(this, _Lutador_Ataque, ataque, "f");
+    };
+    Lutador.prototype.getNome = function () {
+        return __classPrivateFieldGet(this, _Lutador_Nome, "f");
+    };
+    Lutador.prototype.getVida = function () {
+        return __classPrivateFieldGet(this, _Lutador_Vida, "f");
+    };
+    Lutador.prototype.getAtaque = function () {
+        return __classPrivateFieldGet(this, _Lutador_Ataque, "f");
+    };
+    return Lutador;
+}());
+_Lutador_Nome = new WeakMap(), _Lutador_Vida = new WeakMap(), _Lutador_Ataque = new WeakMap();
+var lutador = new Lutador("Arthur", 10, 2), lutador02 = new Lutador("War", 11, 2);
+lutadores.push(lutador, lutador02);
+console.log(lutadores[0].getNome());
--- app.ts
+++ app.ts
@@ -3,36 +3,36 @@
 // barra de chocolate em pedaços 1x1 e retornar o número mínimo de quebras
 // necessárias, se algum dos parâmetros de entrada for 0 você deve retornar -1.
 
-// Calcular(100, 100);
-// function Calcular(Largura: number, altura: number) {
-//   let ContadorQuebras = 0;
-//   const Total = Largura * altura;
-//   for (let index = 1; index < Total; index++) {
-//     Total / 2;
-//     ContadorQuebras++;
-//   }
-//   return console.log(ContadorQuebras);
-// }
-// 2.Faça uma função que receba uma string como parâmetro, essa string pode conter
+Calcular(100, 100);
+function Calcular(Largura: number, altura: number) {
+  let ContadorQuebras = 0;
+  const Total = Largura * altura;
+  for (let index = 1; index < Total; index++) {
+    Total / 2;
+    ContadorQuebras++;
+  }
+  return console.log(ContadorQuebras);
+}
+//  2.Faça uma função que receba uma string como parâmetro, essa string pode conter
 // letras maiúsculas ou minúsculas. A função deve retornar a string apenas com letras
 // minúsculas ou maiúsculas de acordo com a seguinte regra: fazer o mínimo de
 // alterações possíveis, se a contagem de minúsculas e maiúsculas for igual deve
 // retornar apenas minúsculas.
-// function verificando(palavra: string): string {
-//   let maiúsculas = palavra.replace(/[^A-Z\s]/g, ""),
-//     minúsculas = palavra.replace(/[^a-z\s]/g, "");
+function verificando(palavra: string): string {
+  let maiúsculas = palavra.replace(/[^A-Z\s]/g, ""),
+    minúsculas = palavra.replace(/[^a-z\s]/g, "");
 
-//   if (minúsculas.length == maiúsculas.length) {
-//     return palavra.toLocaleLowerCase();
-//   }
-//   if (minúsculas.length < maiúsculas.length) {
-//     return palavra.toUpperCase();
-//   }
-//   if (minúsculas.length > maiúsculas.length) {
-//     return palavra.toLocaleLowerCase();
-//   }
-// }
-// console.log(verificando("artHUR"));
+  if (minúsculas.length == maiúsculas.length) {
+    return palavra.toLocaleLowerCase();
+  }
+  if (minúsculas.length < maiúsculas.length) {
+    return palavra.toUpperCase();
+  }
+  if (minúsculas.length > maiúsculas.length) {
+    return palavra.toLocaleLowerCase();
+  }
+}
+console.log(verificando("artHUR"));
 // 3. Crie uma função que retorne o nome do vencedor em uma luta entre dois lutadores.
 // Cada lutador ataca o adversário no seu próprio turno, quem derrubar o outro
 // primeiro vence, um jogador é derrubado quando sua vida for <= 0.
@@ -41,3 +41,137 @@
 // Tanto vida como ataque devem ser valores inteiros maiores que 0 e a função além
 // dos dois lutadores deve receber um terceiro parâmetro com o nome de quem vai
 // começar atacando.
+let Lutadores: object[] = [];
+let Continuar = true;
+
+class Lutador {
+  #Nome: string;
+  #Vida: number;
+  #Ataque: number;
+
+  constructor(nome: string, vida: number, ataque: number) {
+    this.SetNome(nome);
+    this.SetVida(vida);
+    this.SetAtaque(ataque);
+  }
+
+  SetNome(nome: string): string {
+    return (this.#Nome = nome);
+  }
+
+  SetVida(vida: number): number {
+    return (this.#Vida = vida);
+  }
+
+  SetAtaque(ataque: number): number {
+    return (this.#Ataque = ataque);
+  }
+
+  GetNome(): string {
+    return this.#Nome;
+  }
+
+  GetVida(): number {
+    return this.#Vida;
+  }
+
+  GetAtaque(): number {
+    return this.#Ataque;
+  }
+
+  Atacar(): number {
+    return this.#Ataque;
+  }
+}
+
+while (Continuar) {
+  let Menu = prompt("1 Criar Lutadores // 2 Começar a Batalha // 3 Finalizar");
+
+  switch (Menu) {
+    case "1":
+      CriarLutador();
+      break;
+
+    case "2":
+      Batalha();
+      break;
+
+    case "3":
+      Continuar = false;
+      break;
+
+    default:
+      alert("Opção inválida");
+      break;
+  }
+}
+
+function CriarLutador() {
+  if (Lutadores.length > 1) {
+    return alert("Limite de Lutadores Atingido");
+  }
+
+  let Nome = String(prompt("Qual o Nome do Lutador ?"));
+  let Vida = Number(prompt("Qual a Vida do Lutador ?"));
+  let Ataque = Number(prompt("Qual o Ataque do Lutador ?"));
+
+  let NewLutador = new Lutador(Nome, Vida, Ataque);
+
+  Lutadores.push(NewLutador);
+}
+
+function Batalha() {
+  if (Lutadores.length == 1) {
+    return alert("Cadastre 2 Lutadores antes de começar uma Batalha");
+  }
+
+  let PrimeiroLutador = true;
+  let Vencedor = false;
+  let VencedorSorteio: object = {};
+  let PerdedorSorteio: object = {};
+  let Sorteio = Math.floor(10 * Math.random());
+
+  while (PrimeiroLutador) {
+    let PalpiteLutador1 = Number(prompt("Qual o PalpiteLutador1 do Lutador ?"));
+    let PalpiteLutador2 = Number(prompt("Qual o PalpiteLutador2 do Lutador ?"));
+
+    if (PalpiteLutador1 == Sorteio) {
+      VencedorSorteio = Lutadores[0];
+      PerdedorSorteio = Lutadores[1];
+      alert(`${Lutadores[0].GetNome()} Começa primeiro`);
+      PrimeiroLutador = false;
+    }
+
+    if (PalpiteLutador2 == Sorteio) {
+      VencedorSorteio = Lutadores[1];
+      PerdedorSorteio = Lutadores[0];
+      alert(`${Lutadores[1].GetNome()} Começa primeiro`);
+      PrimeiroLutador = false;
+    }
+  }
+
+  console.log(`${VencedorSorteio.GetNome()} Ataca primeiro !`);
+
+  while (Vencedor == false) {
+    let AtaqueVencedor = VencedorSorteio.Atacar();
+    let AtaquePerdedor = PerdedorSorteio.Atacar();
+
+    console.log(
+      `${VencedorSorteio.GetNome()} Atacou ${PerdedorSorteio.GetNome()} Ataque: ${VencedorSorteio.Atacar()} De Dano`
+    );
+    PerdedorSorteio.SetVida(PerdedorSorteio.GetVida() - AtaqueVencedor);
+    if (VencedorSorteio.GetVida() <= 0) {
+      alert(`Luta finalizada ${PerdedorSorteio.GetNome()} Venceu !`);
+      Vencedor = true;
+    }
+
+    console.log(
+      `${PerdedorSorteio.GetNome()} Atacou ${VencedorSorteio.GetNome()} Ataque: ${PerdedorSorteio.Atacar()} De Dano`
+    );
+    VencedorSorteio.SetVida(VencedorSorteio.GetVida() - AtaquePerdedor);
+    if (PerdedorSorteio.GetVida() <= 0) {
+      alert(`Luta finalizada ${VencedorSorteio.GetNome()} Venceu !`);
+      Vencedor = true;
+    }
+  }
+}
